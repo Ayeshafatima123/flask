c156b0a57a46ebfc57fe1a46fd99d1150fe05b92
Ayeshafatima123 <160357945+Ayeshafatima123@users.noreply.github.com>
2025-11-14 18:54:43 +0500

    Update app.py

--- flask_project/app.py
+++ flask_project/app.py
@@ -49,9 +49,12 @@ def subscribe():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
+
 app = Flask(__name__)
 
 @app.get("/")
 def home():
     return jsonify({"message": "Flask running on Vercel successfully!"})
 
+def handler(event, context):
+    return app(event, context)
